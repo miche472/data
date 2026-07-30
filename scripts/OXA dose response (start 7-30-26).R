cab4624790d571fa45949676c8c46d6db43c28fc
miche472 <miche472@umn.edu>
2026-07-30 11:43:49 -0500

    Create OXA dose response (start 7-30-26).R

---
+++ scripts/OXA dose response (start 7-30-26).R
@@ -0,0 +1,74 @@
+#Orexin A icv dose response (July/August 2026)
+
+#Started: 7-30-26
+#Revised: 
+
+#Libraries####
+library(dplyr) #to open a RDS and use pipe
+library(tidyr) #to use cumsum
+library(ggplot2)
+library(readr)
+library(lmerTest)
+library(emmeans)
+library(ggpubr)
+library(ggrepel) # optional, but better for labels
+library(slider)
+library(lubridate)
+library(lme4)
+library(hms)
+
+
+pacman::p_load(
+  tidyverse,
+  googledrive,
+  furrr,
+  zoo,
+  robustlmm,
+  mmand
+)
+
+#Format plot (LM version 2) ####
+format.plot_LM2 <- theme(
+  strip.background = element_blank(),
+  panel.spacing.x = unit(0.1, "lines"),          
+  panel.spacing.y = unit(1.5, "lines"), 
+  panel.border = element_blank(),
+  panel.grid.minor = element_blank(), # remove background grid lines only
+  panel.grid.major = element_blank(),
+  axis.line = element_line(color = "black"),
+  plot.title = element_text(size=17, hjust = 0.5, face="bold", vjust=2),
+  legend.title=element_text(size=15, face="bold"),
+  legend.text=element_text(size=13),
+  axis.title.x = element_text(face="bold", size= 15),
+  axis.text.x = element_text(size= 13, angle=25, vjust=0.5, hjust=0.7),
+  axis.title.y = element_text(face="bold", size= 15),
+  axis.text.y = element_text(size = 13))
+# Define custom colors
+custom_colors1_GLP_Exp2 <- c("Tirzepatide" = "#1e6deb", "Vehicle" = "#403d3c")
+custom_colors2_GLP_Exp2 <- c("Tirzepatide" = "#1E90FF", "Vehicle" = "#8B8989")
+custom_colors3_GLP_Exp2 <- c("Tirzepatide" = "#104E8B", "Vehicle" = "#403d3c")
+
+#Read in Sable data
+sable_dwn <- readRDS(file = "../data/sable_downsampled_data.rds") 
+
+#Functions####
+#This method gives integer values for zt_time and accounts for the 6:30 to 18:30 cycle
+#It is more explicit in its format/logic compared to original zt_time function
+zt_time <- function(DateTime){
+  time <- hour(DateTime) + minute(DateTime)/60
+  zt <- time - 18.5
+  zt <- if_else(zt < 0, zt + 24, zt)
+  floor(zt)}
+
+# 0.5-2.5hrs after injection...cumulative
+# 0.5-4.5hrs after injection...cumulative 
+# 0.5-24.5hrs after injection...cumulative
+
+#TEE, RMR and SPA
+#Calculate hourly basis --> use photoperiod code from Exp 2a Photo EE & RQ
+
+#Locomotion: time spent moving and distance traveled
+#Look at resulting formatting and decide how to append dose information. Could hardcode, but there may be a better approach
+
+# FOr group assignment I should read in the .csv file that i created in the group assignment script
+# use the one from 7-17 for all mice except 3738. Use the one from 7-30 for 3738
